a9f338967e93623ff8dd8181d9c3ad7a3ec35808
Nathan Ganser <me@nathanganser.com>
2021-04-16 07:33:01 +0100

    initial commit

--- src/services/providers/helpers/skyIdHelper.js
+++ src/services/providers/helpers/skyIdHelper.js
@@ -38,7 +38,6 @@ let skyid = new SkyID('StackEdit', async (message) => {
           imageUrl: user.avatar || '',
         });
 
-        // Build token object including scopes and sub
         const token = {
           name: user.username,
           sub: `${id}`,
@@ -69,14 +68,8 @@ export default {
   async addAccount(noteAccess = true) {
     done = false;
     await skyid.sessionStart();
-
-    // while (!done){
-    //   setTimeout(function(){
-    //   },1000);
-    // }
     const id = await skyid.getId();
     return {sub: id};
-    //await this.fetchNote();
   },
 
   async getId(){
@@ -93,31 +86,25 @@ export default {
     console.log("fetch test:" + workspace);
     let respObs = '';
 		await skyid.getFile(workspace, function(response, revision) {
-			if (response == '') { // file not found
+			if (response == '') {
         respObs = [];
-			} else { // success
+			} else {
 				respObs = JSON.parse(response).files
 			}
       console.log("found")
       console.log(respObs);
 		})
-    //console.log("found")
-    //console.log(respObs);
     if(!filename) return respObs
     return respObs.filter(file => file.filename == filename && file.type == type && file.parent == parent && file.id == id)[0];
-    //return respObs.filter(file => file.id == id)[0];
 	},
 
   async uploadNote({token, file = null, filename, skyId, workspace, type = null, id, parent = null, remove = null}) {
     console.log("at upload");
-		//var note = document.getElementById('note').value
     var old = await this.downloadNote({workspace: workspace});
-    //old = old.filter(it => !(it.filename == filename && it.type == type && it.parent == parent));
     old = old.filter(it => !(it.id == id));
     if(!remove) old.push({filename: filename, file: file, type: type, id: id, parent: parent});
     console.log("loc " + filename);
     console.log(old);
-    //old = old.filter(file => file.filename != filename);
 		var json = JSON.stringify({files: old});
 		await skyid.setFile(workspace, json, function(response) {
       console.log("done " + filename);
